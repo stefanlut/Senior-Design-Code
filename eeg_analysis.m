d656e7de9a7a64afb1819e8794667d185e2e91ba
AllanG0404 <103614797+AllanG0404@users.noreply.github.com>
2022-04-15 12:34:30 -0400

    Update eeg_analysis.m

--- eeg_analysis.m
+++ eeg_analysis.m
@@ -66,47 +66,104 @@ for i = 1:size(EEG.data,1)
        xlim([0 60]);
 end
 sgtitle('PSD Across Eyes Open Period');
+
 %% Open vs Close
 close all;
 EEGvsRef = struct();
-EEGvsRef.Oz = struct();
 
 eyes_closed1 = 470601;
 eyes_open1 = 626501;
 eyes_closed2 = 782001;
 
-[EEGvsRef.Oz.yclose1 , EEGvsRef.Oz.xclose1] = pwelch(downsample(EEG.data(22,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
-figure;subplot(421);
-plot(EEGvsRef.Oz.xclose1, 10*log10(EEGvsRef.Oz.yclose1),'r'); grid on; xlim([0 60]); title("Oz eyes closed"); xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+% [EEGvsRef.Oz.yclose1 , EEGvsRef.Oz.xclose1] = pwelch(downsample(EEG.data(22,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+% figure; subplot(2,2,1);
+% plot(EEGvsRef.Oz.xclose1, 10*log10(EEGvsRef.Oz.yclose1),'r'); grid on; xlim([0 20]); title("EEG Channels- Eyes Closed"); xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+% legend('Oz');
+% 
+% subplot(2,2,2);
+% [EEGvsRef.Oz.yclose2 , EEGvsRef.Oz.xclose2] = pwelch(downsample(EEG.data(22,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate); 
+% plot(EEGvsRef.Oz.xclose2, 10*log10(EEGvsRef.Oz.yclose2),'r'); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)"); title("EEG Channels- Eyes Open");xlim([0 20]);
+% legend('Oz');
 
-subplot(422);
-[EEGvsRef.Oz.yclose2 , EEGvsRef.Oz.xclose2] = pwelch(downsample(EEG.data(22,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate); 
-plot(EEGvsRef.Oz.xclose2, 10*log10(EEGvsRef.Oz.yclose2),'r'); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)"); title("Oz eyes open");xlim([0 60]);
+eegchannels=["Oz"];
+for i=1:length(eegchannels)
+    EEGvsRef.(eegchannels{i}) = struct();
+ 
+    subplot(221); 
+    [EEGvsRef.(eegchannels{i}).yclose1, EEGvsRef.(eegchannels{i}).xclose1] = pwelch(downsample(EEG.data(find(strcmp(string(names),eegchannels{i})),eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+    plot(EEGvsRef.(eegchannels{i}).xclose1, 10*log10(EEGvsRef.(eegchannels{i}).yclose1));
+    if i==length(eegchannels)
+        legend('Oz'); title("EEG Channels- Eyes Closed");xlim([0 20]); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+    end
+    hold on
+    
+     subplot(222); 
+     [EEGvsRef.(eegchannels{i}).yclose2, EEGvsRef.(eegchannels{i}).xclose2] = pwelch(downsample(EEG.data(find(strcmp(string(names),eegchannels{i})),eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+     plot(EEGvsRef.(eegchannels{i}).xclose2, 10*log10(EEGvsRef.(eegchannels{i}).yclose2));
+   
+    if i==length(eegchannels)
+        legend('Oz'); title("EEG Channels- Eyes Open");xlim([0 20]); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+    end
+    hold on
+end
 
-EEGvsRef.O1 = struct();
-EEGvsRef.O2 = struct();
-EEGvsRef.POz = struct();
+refchannels=["O1","O2","POz"]; 
+for i=1:length(refchannels)
+    EEGvsRef.(refchannels{i}) = struct();
+ 
+    subplot(223); 
+    [EEGvsRef.(refchannels{i}).yclose1, EEGvsRef.(refchannels{i}).xclose1] = pwelch(downsample(EEG.data(find(strcmp(string(names),refchannels{i})),eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+    plot(EEGvsRef.(refchannels{i}).xclose1, 10*log10(EEGvsRef.(refchannels{i}).yclose1));
+    if i==length(refchannels)
+        legend('O1','O2','POz'); title("Ref Channels- Eyes Closed");xlim([0 20]); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+    end
+    hold on
+    
+     subplot(224); 
+     [EEGvsRef.(refchannels{i}).yclose2, EEGvsRef.(refchannels{i}).xclose2] = pwelch(downsample(EEG.data(find(strcmp(string(names),refchannels{i})),eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+     plot(EEGvsRef.(refchannels{i}).xclose2, 10*log10(EEGvsRef.(refchannels{i}).yclose2));
+   
+    if i==length(refchannels)
+        legend('O1','O2','POz'); title("Ref Channels- Eyes Open");xlim([0 20]); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+    end
+    hold on
+end
 
-subplot(423);
-[EEGvsRef.O1.yclose1, EEGvsRef.O1.xclose1] = pwelch(downsample(EEG.data(9,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);title("O1 eyes closed");xlim([0 60]);
-plot(EEGvsRef.O1.xclose1, 10*log10(EEGvsRef.O1.yclose1)); grid on; xlim([0 60]); title("O1 eyes closed"); xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
 
-subplot(424);
-[EEGvsRef.O1.yclose2, EEGvsRef.O1.xclose2] = pwelch(downsample(EEG.data(9,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);title("O1 eyes open");xlim([0 60]);
-plot(EEGvsRef.O1.xclose2, 10*log10(EEGvsRef.O1.yclose2)); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)"); title("O1 eyes open");xlim([0 60]);
 
-subplot(425);
-[EEGvsRef.O2.yclose1, EEGvsRef.O2.xclose1] = pwelch(downsample(EEG.data(10,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);title("O2 eyes closed");xlim([0 60]);
-plot(EEGvsRef.O2.xclose1, 10*log10(EEGvsRef.O2.yclose1)); grid on; xlim([0 60]); title("O2 eyes closed"); xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
 
-subplot(426);
-[EEGvsRef.O2.yclose2, EEGvsRef.O2.xclose2] = pwelch(downsample(EEG.data(10,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);title("O2 eyes open");xlim([0 60]);
-plot(EEGvsRef.O2.xclose2, 10*log10(EEGvsRef.O2.yclose2)); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)"); title("O2 eyes open");xlim([0 60]);
 
-subplot(427);
-[EEGvsRef.POz.yclose1 , EEGvsRef.POz.xclose1] = pwelch(downsample(EEG.data(21,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);title("POz eyes closed");xlim([0 60]);
-plot(EEGvsRef.POz.xclose1, 10*log10(EEGvsRef.POz.yclose1)); grid on; xlim([0 60]); title("POz eyes closed"); xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+% 
+% EEGvsRef.O1 = struct();
+% EEGvsRef.O2 = struct();
+% EEGvsRef.POz = struct();
+% 
+% subplot(223);
+% [EEGvsRef.O1.yclose1, EEGvsRef.O1.xclose1] = pwelch(downsample(EEG.data(9,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+% plot(EEGvsRef.O1.xclose1, 10*log10(EEGvsRef.O1.yclose1));
+% hold on
+% 
+% [EEGvsRef.O2.yclose1, EEGvsRef.O2.xclose1] = pwelch(downsample(EEG.data(10,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+% plot(EEGvsRef.O2.xclose1, 10*log10(EEGvsRef.O2.yclose1));  
+% hold on
+% 
+% [EEGvsRef.POz.yclose1 , EEGvsRef.POz.xclose1] = pwelch(downsample(EEG.data(21,eyes_closed1:eyes_open1),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+% plot(EEGvsRef.POz.xclose1, 10*log10(EEGvsRef.POz.yclose1));
+% legend('O1','O2','POz'); title("Ref Channels- Eyes Closed");xlim([0 20]); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
+% 
+% 
+% 
+% subplot(224);
+% [EEGvsRef.O1.yclose2, EEGvsRef.O1.xclose2] = pwelch(downsample(EEG.data(9,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+% plot(EEGvsRef.O1.xclose2, 10*log10(EEGvsRef.O1.yclose2)); 
+% hold on
+% 
+% 
+% [EEGvsRef.O2.yclose2, EEGvsRef.O2.xclose2] = pwelch(downsample(EEG.data(10,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+% plot(EEGvsRef.O2.xclose2, 10*log10(EEGvsRef.O2.yclose2)); 
+% hold on
+% 
+% [EEGvsRef.POz.yclose2 , EEGvsRef.POz.xclose2] = pwelch(downsample(EEG.data(21,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);
+% plot(EEGvsRef.POz.xclose2, 10*log10(EEGvsRef.POz.yclose2));
+% legend('O1','O2','POz'); title("Ref Channels- Eyes Open");xlim([0 20]); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)")
 
-subplot(428);
-[EEGvsRef.POz.yclose2 , EEGvsRef.POz.xclose2] = pwelch(downsample(EEG.data(21,eyes_open1:eyes_closed2),EEG.srate/newEEG1.srate),[],[],[],newEEG1.srate);title("POz eyes open");xlim([0 60]);
-plot(EEGvsRef.POz.xclose2, 10*log10(EEGvsRef.POz.yclose2)); grid on; xlabel("Frequency (Hz)"); ylabel("Power/Frequency (dB/Hz)"); title("POz eyes open");xlim([0 60]);
